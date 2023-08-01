a0169c2d74a6107a8b65deb93fa4662177e3b356
riyas pullur <riyaspullurofficial@gmail.com>
2023-08-01 06:10:28 +0530

    dd

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -28,10 +28,12 @@ class HomeScreen extends StatelessWidget {
             width: MediaQuery.of(context).size.width,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
                   height: 50,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -107,6 +109,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Container(
                   height: 1.0,
+                  margin: const EdgeInsets.only(bottom: 25),
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
                     color: const Color(0xFFffffff),
@@ -122,6 +125,120 @@ class HomeScreen extends StatelessWidget {
                       )
                     ],
                   ),
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 150,
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          main_bannerIcon,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Container(
+                        height: 10,
+                        width: MediaQuery.of(context).size.width,
+                        child: ListView.builder(
+                            itemCount: 3,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (cnx, index) {
+                              return index == 0
+                                  ? Container(
+                                      margin: const EdgeInsets.only(right: 5),
+                                      height: 10,
+                                      width: 10,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                    )
+                                  : Container(
+                                      margin: const EdgeInsets.only(right: 5),
+                                      height: 10,
+                                      width: 10,
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          border: Border.all(
+                                              width: 1, color: Colors.white),
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                    );
+                            }),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 10,
+                        width: MediaQuery.of(context).size.width,
+                        child: ListView.builder(
+                            itemCount: 3,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (cnx, index) {
+                              return index == 0
+                                  ? Container(
+                                      margin: const EdgeInsets.only(right: 5),
+                                      height: 10,
+                                      width: 10,
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                    )
+                                  : Container(
+                                      margin: const EdgeInsets.only(right: 5),
+                                      height: 10,
+                                      width: 10,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          border: Border.all(
+                                              width: 1, color: Colors.black),
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                    );
+                            }),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 100,
+                      itemBuilder: (cnx, index) {
+                        return Container(
+                          height: 50,
+                          width: 50,
+                          margin: const EdgeInsets.only(right: 30),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              gradient: const LinearGradient(colors: [
+                                Color(0x3a0A6AA1),
+                                Color(0x3a2BA6C4)
+                              ])),
+                          child: Center(
+                            child: Image.asset(
+                              groupiconIcon,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        );
+                      }),
                 ),
               ],
             ),
